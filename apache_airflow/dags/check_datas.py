197215ae145d8f21f8905779b65aba7d8bd5dd50
vincentnam <dang.vincentnam@gmail.com>
2020-08-03 20:08:12 +0200

    DONE : log of data processed and update of data document operation in MongoDB metadatabase + refactoring / code cleaning

--- apache_airflow/dags/check_datas.py
+++ apache_airflow/dags/check_datas.py
@@ -1,16 +1,14 @@
 # The DAG object; we'll need this to instantiate a DAG
 from airflow import DAG
 # Operators; we need this to operate!
-
 from airflow.operators.python_operator import PythonOperator, \
     BranchPythonOperator
 from airflow.utils.dates import days_ago
-from airflow.contrib.hooks.mongo_hook import MongoHook
 from datetime import timedelta
 from airflow.operators.dummy_operator import DummyOperator
-from airflow.utils.helpers import chain
 import logging
 from datetime import datetime
+
 globals()["META_MONGO_IP"] = "141.115.103.31"
 globals()["OPENSTACK_SWIFT_IP"] = "141.115.103.30"
 globals()["GOLD_MONGO_IP"] = "141.115.103.33"
@@ -27,12 +25,11 @@ globals()["SWIFT_KEY"] = 'testing'
 globals()["MONGO_META_CONN_ID"] = "mongo_metadatabase"
 globals()["MONGO_GOLD_CONN_ID"] = "mongo_gold"
 
-
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
     'start_date': days_ago(2),
-    'provide_context' : True
+    'provide_context': True
 
 }
 dag = DAG(
@@ -45,14 +42,14 @@ dag = DAG(
 )
 
 data_account = {
-    "neocampus" : {
+    "neocampus": {
         "application/json": "new_input"
     },
-    "mygates" : {
+    "mygates": {
         "image/jpeg": "new_input",
         "image/png": "new_input"
     },
-    "default" : {
+    "default": {
         "image/jpeg": "new_input",
         "image/png": "new_input",
         "application/json": "new_input"
@@ -61,13 +58,17 @@ data_account = {
 
 from airflow.exceptions import AirflowSkipException
 from airflow.api.common.experimental.trigger_dag import trigger_dag
+from pymongo import MongoClient
 
 
 def get_list_mongo_meta(**kwargs):
-    meta_base = MongoHook(globals()["MONGO_META_CONN_ID"])
-    data_to_process_list = meta_base.get_conn().stats.get_collection("swift").find_one({"type":"data_to_process_list"})
-
-
+    meta_base = MongoClient(
+        "mongodb://" + globals()["META_MONGO_IP"] + ":" + globals()[
+            "MONGO_PORT"] + "/"
+    )
+    # meta_base = MongoHook(globals()["MONGO_META_CONN_ID"])
+    data_to_process_list = meta_base.stats["swift"].find_one(
+        {"type": "data_to_process_list"})
     swift_data_list = data_to_process_list["data_to_process"]
 
     for data_doc in swift_data_list:
@@ -75,14 +76,69 @@ def get_list_mongo_meta(**kwargs):
                                   data_doc["swift_container"],
                                   data_doc["swift_id"],
                                   datetime.utcnow().replace(
-            microsecond=0).isoformat())
-        trigger_dag(dag_id = data_account[data_doc["swift_container"]][data_doc["content_type"]],
-                    run_id = run_id,
-                    conf = data_doc)
+                                      microsecond=0).isoformat())
+        trigger_dag(dag_id=data_account[data_doc["swift_container"]][
+            data_doc["content_type"]],
+                    run_id=run_id,
+                    conf=data_doc)
         logging.info('triggering dag %s with %s' % (run_id, data_doc))
-
+        meta_base.stats["swift"].find_one_and_update(
+            {"type": "data_to_process_list"},
+            {
+                "$pop":
+                    {"data_to_process": -1}
+            }
+        )
+        today = datetime.date.today()
+        tomorrow = today + datetime.timedelta(days=1)
+        today = datetime.datetime(today.year, today.month, today.day)
+        tomorrow = datetime.datetime(tomorrow.year,
+                                     tomorrow.month,
+                                     tomorrow.day)
+        if meta_base.stats["data_log"].find_one(
+                {
+                    "date":
+                        {
+                            "$gte": today,
+                            "$lt": tomorrow
+                        }
+                }
+        ) is None:
+            meta_base.stats["data_log"].insert_one(
+                {
+                    "date": today,
+                    "data_processed":
+                        [
+                            {
+                                "swift_id": data_doc["swift_id"],
+                                "swift_container": data_doc["swift_container"],
+                                "content_type": data_doc["content_type"]
+                            }
+                        ]
+                }
+            )
+        else:
+            meta_base.stats["data_log"].find_one_and_update(
+                {
+                    "date":
+                        {
+                            "$gte": today,
+                            "$lt": tomorrow
+                        }
+                },
+                {
+                    "$push": {
+                        "data_processed": {
+                            "swift_id": data_doc["swift_id"],
+                            "swift_container": data_doc["swift_container"],
+                            "content_type": data_doc["content_type"]
+                        }
+                    }
+                }
+            )
         return
     raise AirflowSkipException('No external dags triggered')
+
 
 done = DummyOperator(
     task_id='Dag_triggered',
@@ -90,11 +146,9 @@ done = DummyOperator(
     dag=dag,
 )
 
-
-
 get_data = PythonOperator(task_id="Get_list",
-                provide_context=True,
-                python_callable=get_list_mongo_meta,
-                start_date=days_ago(2))
+                          provide_context=True,
+                          python_callable=get_list_mongo_meta,
+                          start_date=days_ago(2))
 
 dag >> get_data >> done
