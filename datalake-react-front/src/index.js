e499657dc0bf08d47fc6808a29daafc482f02687
Latamen <latamen.ait.meddour@modis.com>
2021-04-21 15:15:08 +0200

    added components and dependencies

--- datalake-react-front/src/index.js
+++ datalake-react-front/src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Home } from './components/Home';
+import { Header } from './components/Header';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Header />
+    <Home />
   </React.StrictMode>,
   document.getElementById('root')
 );
