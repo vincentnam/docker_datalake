f329af06b8329e30deee91c05663dd2663227bcf
Théo Legagneur <82022563+TheoLGG-Modis@users.noreply.github.com>
2021-04-26 17:13:05 +0200

    Start front download

--- datalake-react-front/src/components/Download.js
+++ datalake-react-front/src/components/Download.js
@@ -7,7 +7,26 @@ export class Download extends React.Component {
             <div>
                 <Header />
                 <div class="p-4">
-                    <p>Download</p>
+                    <h4>Stockage de données</h4>
+                    <form>
+                        <div class="form-group">
+                            <label for="inputState">Type de métadonnée</label>
+                            <select id="inputState" class="form-control">
+                                <option selected>Choose...</option>
+                                <option>...</option>
+                            </select>
+                        </div>
+                        
+                        <div class="mb-3">
+                            <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
+                            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleFormControlFile1">Example file input</label>
+                            <input type="file" class="form-control-file" id="exampleFormControlFile1" />
+                        </div>
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </form>
                 </div>
             </div>
         );
