a67d87cb24691018643e493ea6e2cbae2c81edee
Théo Legagneur <82022563+TheoLGG-Modis@users.noreply.github.com>
2021-04-27 11:58:14 +0200

    Front done
    Front fait, reste a faire le traitement et l'envoi

--- datalake-react-front/src/components/Download.js
+++ datalake-react-front/src/components/Download.js
@@ -1,37 +1,77 @@
 import React from 'react';
 import { Header } from './Header';
-import Dropzone  from 'react-dropzone';
+import Dropzone from 'react-dropzone';
 
 export class Download extends React.Component {
+    constructor() {
+        super();
+        this.onDrop = (files) => {
+            this.setState({files})
+        };
+        this.state = {
+            files: [],
+            meta: '',
+            metier: 'meteo',
+            file: [],
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+
+        this.setState({
+        [name]: value
+        });
+    }
+    
+    handleSubmit(event) {
+        alert(this.state.metier +' '+ this.state.meta + ' '+ JSON.stringify(this.state.files));
+        event.preventDefault();
+    }
+
     render() {
+        const files = this.state.files.map(file => (
+            <li key={file.name}>
+                {JSON.stringify(file)}
+            </li>
+        ));
+
         return(
             <div>
                 <Header />
                 <div class="p-4">
                     <h4>Stockage de données</h4>
-                    <form>
+                    <form onSubmit={this.handleSubmit}>
                         <div class="form-group">
-                            <label for="inputState">Type de métadonnée</label>
-                            <select id="inputState" class="form-control">
-                                <option selected>Choose...</option>
-                                <option>...</option>
+                            <label>Type de métadonnée</label>
+                            <select value={this.state.metier} onChange={this.handleChange} name="metier" class="form-control">
+                                <option value="meteo">Météo</option>
+                                <option value="capteur">Capteur</option>
+                                <option selected value="camera">Camera</option>
                             </select>
                         </div>
                         
                         <div class="mb-3">
-                            <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
-                            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                            <label class="form-label">Métadonnée</label>
+                            <textarea value={this.state.meta} onChange={this.handleChange} name="meta" class="form-control" rows="3" />
                         </div>
                         <div class="form-group">
-                            <label for="exampleFormControlFile1">Example file input</label>
-                            <Dropzone onDrop={acceptedFiles => console.log(acceptedFiles)}>
+                            <Dropzone value={this.state.file} onChange={this.handleChange} name="file" onDrop={this.onDrop}>
                                 {({getRootProps, getInputProps}) => (
-                                    <section>
-                                    <div class="drop d-flex justify-content-center align-items-center" {...getRootProps()}>
+                                <section>
+                                    <div {...getRootProps({className: 'drop d-flex justify-content-center'})}>
                                         <input {...getInputProps()} />
-                                        <p>Veuillez cliquer ou glisser votre fichier.</p>
+                                        <p>Drag 'n' drop veuillez glisser un fichier ou click pour ajouter un fichier.</p>
                                     </div>
-                                    </section>
+                                    <aside>
+                                        <h5>Files</h5>
+                                        <ul>{files}</ul>
+                                    </aside>
+                                </section>
                                 )}
                             </Dropzone>
                         </div>
