03fa7b86e767004c0b83b80299abdf543d89988c
Théo Legagneur <82022563+TheoLGG-Modis@users.noreply.github.com>
2021-04-30 17:35:06 +0200

    Test route storage API

--- datalake-backend/flask/app.py
+++ datalake-backend/flask/app.py
@@ -1,11 +1,23 @@
-from flask import Flask
-
+from flask import Flask, request
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
     return 'Hello Modis!'
+
+@app.route('/storage', methods= ['GET', 'POST'])
+def storage(self):
+    idType = request.form['idType']
+    print(idType)
+    data =  request.form['data']
+    print(data)
+    premieremeta =  request.form['premieremeta']
+    print(premieremeta)
+    deuxiememeta =  request.form['deuxiememeta']
+    print(deuxiememeta)
+    
+    return 'storage'
 
 
 if __name__ == '__main__':
