4f1c3c1d27f879580cafa7072b3890f41f24d41b
sabriBoussetha <sabriBoussetha@users.noreply.github.com>
2021-05-07 14:09:51 +0200

    Revert "added API route to get metadata - selection items and pagination"
    This reverts commit d263165416aa1a4a605f2b44d63ac96069f447b9.

--- datalake-react-front/src/components/download-page/Download.js
+++ datalake-react-front/src/components/download-page/Download.js
@@ -1,15 +1,9 @@
 import React from "react";
 import { Header } from '../Header';
 import { RowItem } from './RowItem';
-import axios from 'axios';
-import ReactPaginate from 'react-paginate';
-import $ from 'jquery';
 
 export class Download extends React.Component {
     selectedElements = []
-    elements = {}
-    url = "http://127.0.0.1:5000/raw-data"
-    perPage = 3
 
     constructor(props) {
         super(props);
@@ -20,30 +14,17 @@ export class Download extends React.Component {
 
         // Set some state
         this.state = {
-            selectedElement: {},
-            elements: {},
-            offset: 0
+            selectedElement: {}
         };
 
-        /*this.elements = { 
+        this.elements = { 
             0: { id: 2, title: 4, metadata: 7 }, 
             1: { id: 0, title: 10, metadata: 0}, 
             2: { id: 0, title: 0, metadata: 0 }, 
             3: { id: 0, title: 0, metadata: 0 }, 
             4: { id: 0, title: 0, metadata: 0 }, 
             5: { id: 0, title: 0, metadata: 0 } 
-        }*/
-
-
-    }
-
-    componentDidMount(){
-        axios.get(this.url)
-            .then(res => {
-                this.setState(
-                    { elements: res.data }
-                );
-            })
+        }
     }
 
     getElements() {
@@ -58,7 +39,6 @@ export class Download extends React.Component {
 
          // if checked
          if(event.target.checked) {
-            //this.props.selectedElements.push(selectedElement)
             this.selectedElements.push(selectedElement)
 
         } else {
@@ -73,46 +53,8 @@ export class Download extends React.Component {
         console.log(selectedElements)
     }
 
-    componentDidMount() {
-        this.loadObjectsFromServer();
-      }
-
-    handlePageClick = (data) => {
-        let selected = data.selected;
-        let offset = Math.ceil(selected * this.perPage);
-    
-        this.setState({ offset: offset }, () => {
-          this.loadObjectsFromServer();
-        });
-      };
-
-    loadObjectsFromServer() {
-        $.ajax({
-          url: this.url,
-          data: { limit: this.perPage, offset: this.state.offset },
-          dataType: 'json',
-          type: 'GET',
-    
-          success: (data) => {
-            this.setState({
-              elements: data.result.objects,
-              pageCount: Math.ceil(data.result.length / this.perPage),
-            });
-          },
-    
-          error: (xhr, status, err) => {
-            console.error(this.url, status, err.toString()); // eslint-disable-line
-          },
-        });
-      }
-
     render() {
-        let elts = []
-        console.log(this.state.elements)
-        if(this.state.elements){
-            elts = this.state.elements
-            console.log(elts)
-        }
+        let elts = this.getElements()
         let selectedElements = this.state.selectedElements
         let handler = this.handler
 
@@ -124,45 +66,19 @@ export class Download extends React.Component {
                     <table class="table">
                         <thead>
                             <tr>
-                                <th scope="col">Id objet Swift</th>
-                                <th scope="col">Utilisateur Swift</th>
-                                <th scope="col">Nom de l'objet</th>
-                                <th scope="col">Date de création</th>
-                                <th scope="col"></th>
+                            <th scope="col">Id</th>
+                            <th scope="col">Titre</th>
+                            <th scope="col">Métadonnées</th>
+                            <th scope="col"></th>
                             </tr>
                         </thead>
                         <tbody>
-                                
-                            { Object.keys(elts).map(function(key, index){ 
-            
-                                return <RowItem key={index} item={elts[key]} handler={handler} />
 
-                            }) }
+                        { Object.keys(elts).map(function(key, index){ 
+        
+                            return <RowItem key={index} item={elts[key]} handler={handler} />
 
-                            <div class="commentBox">
-
-                                <ReactPaginate
-                                    previousLabel={'previous'}
-                                    nextLabel={'next'}
-                                    breakLabel={'...'}
-                                    breakClassName={'break-me'}
-                                    pageCount={this.state.pageCount}
-                                    marginPagesDisplayed={2}
-                                    pageRangeDisplayed={5}
-                                    onPageChange={this.handlePageClick}
-                                    containerClassName={'pagination'}
-                                    activeClassName={'active'}
-                                    breakClassName={'page-item'}
-                                    breakLinkClassName={'page-link'}
-                                    containerClassName={'pagination'}
-                                    pageClassName={'page-item'}
-                                    pageLinkClassName={'page-link'}
-                                    previousClassName={'page-item'}
-                                    previousLinkClassName={'page-link'}
-                                    nextClassName={'page-item'}
-                                    nextLinkClassName={'page-link'}
-                                />
-                            </div>
+                           }) }
                                
                         </tbody>
                     </table>
