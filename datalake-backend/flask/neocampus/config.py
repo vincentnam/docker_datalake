8b828706ad6a0f45d2a0e1a6e9a328f5ddfeb7c1
Théo Legagneur <82022563+TheoLGG-Modis@users.noreply.github.com>
2021-05-10 14:37:42 +0200

    delete

--- datalake-backend/flask/neocampus/config.py
+++
@@ -1,7 +0,0 @@
-MONGO_URL = "10.200.156.254:27017"
-
-SWIFT_AUTHURL = "http://10.200.156.252:8080/auth/v1.0"
-SWIFT_USER = 'test:tester'
-SWIFT_KEY = 'testing'
-
-SWIFT_FILES_DIRECTORY = "cache-swift-files"
