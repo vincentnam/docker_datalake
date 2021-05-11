36f4f0da3bbb2fe3f3eb102231e2770ded54df20
Latamen <latamen.ait.meddour@modis.com>
2021-05-11 18:54:07 +0200

    change route method and added parameters

--- datalake-backend/flask/neocampus/services/mongo.py
+++ datalake-backend/flask/neocampus/services/mongo.py
@@ -18,13 +18,28 @@ def get_swift_original_object_name(swift_container_name, swift_object_id):
     return metadata_swift.get('original_object_name')
 
 
-def get_collections(db_name, offset, limit):
+def get_metadata(db_name, params):
+    print(params['offset'])
+    print(params['limit'])
     mongodb_url = current_app.config['MONGO_URL']
     mongo_client = MongoClient(mongodb_url, connect=False)
     mongo_db = mongo_client.swift
     collection = mongo_db[db_name]
 
-    nb_objects = collection.find().count()
-    collections = collection.find().skip(offset).limit(limit)
+    metadata = collection.find()
 
-    return nb_objects, collections
+    nb_objects = collection.find().count()
+
+    if(params['filetype']):
+        print('filetype')
+        metadata = collection.find({"content_type": params['filetype']})
+
+    if(params['offset']):
+        print('offset')
+        metadata = metadata.skip(params['offset'])
+
+    if(params['limit']):
+        print('limit')
+        metadata = metadata.limit(params['limit'])
+
+    return nb_objects, metadata
