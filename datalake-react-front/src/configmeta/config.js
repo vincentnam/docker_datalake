4d2902cd864b712ee853534aa9b1eea932fd4e31
TheoLGG <tlegagneur@myges.fr>
2021-06-10 16:58:36 +0200

    #55 Change file config metadata

--- datalake-react-front/src/configmeta/config.js
+++ datalake-react-front/src/configmeta/config.js
@@ -3,65 +3,153 @@ export const config = {
         {
             "id": 0,
             "label": "Veuillez choisir votre type de métadonnée ...",
+            "type_file_accepted": [],
             "metadonnees": [
             ]
         },
         {
             "id": 1,
-            "label": "Météo",
+            "label": "Texte",
+            "type_file_accepted": ["text/plain", "application/csv", "application/vnd.ms-excel"],
             "metadonnees": [
                 {
                     "id": 1,
-                    "label": "Métadonnée 1",
+                    "label": "Métadonnée Texte 1",
                     "type": "text",
-                    "name": "meta1",
+                    "name": "metatest1",
                     "value": "",
                 },
                 {
                     "id": 2,
-                    "label": "Métadonnée 2",
+                    "label": "Métadonnée Texte 2",
                     "type": "text",
-                    "name": "meta2",
+                    "name": "metatest2",
                     "value": "",
                 }
             ]
         },
         {
             "id": 2,
-            "label": "Capteur",
+            "label": "Image",
+            "type_file_accepted": ["image/png", "image/jpeg"],
             "metadonnees": [
                 {
                     "id": 1,
-                    "label": "Métadonnée 1",
-                    "type": "textarea",
-                    "name": "meta1",
+                    "label": "Métadonnée Image 1",
+                    "type": "text",
+                    "name": "metaimage1",
                     "value": "",
                 },
                 {
                     "id": 2,
-                    "label": "Métadonnée 2",
+                    "label": "Métadonnée Image 2",
                     "type": "text",
-                    "name": "meta2",
-                    "value": "",
-                },
-                {
-                    "id": 3,
-                    "label": "Métadonnée 3",
-                    "type": "number",
-                    "name": "meta3",
+                    "name": "metaimage2",
                     "value": "",
                 }
             ]
         },
         {
             "id": 3,
-            "label": "Autre métadonnée",
+            "label": "Son",
+            "type_file_accepted": [],
             "metadonnees": [
                 {
                     "id": 1,
-                    "label": "Métadonnée 1",
-                    "type": "textarea",
-                    "name": "meta1",
+                    "label": "Métadonnée Son 1",
+                    "type": "text",
+                    "name": "metason1",
+                    "value": "",
+                },
+                {
+                    "id": 2,
+                    "label": "Métadonnée Son 2",
+                    "type": "text",
+                    "name": "metason2",
+                    "value": "",
+                }
+            ]
+        },
+        {
+            "id": 4,
+            "label": "Vidéo",
+            "type_file_accepted": [],
+            "metadonnees": [
+                {
+                    "id": 1,
+                    "label": "Métadonnée Vidéo 1",
+                    "type": "text",
+                    "name": "metavidéo1",
+                    "value": "",
+                },
+                {
+                    "id": 2,
+                    "label": "Métadonnée Vidéo 2",
+                    "type": "text",
+                    "name": "metavidéo2",
+                    "value": "",
+                }
+            ]
+        },
+        {
+            "id": 5,
+            "label": "Tuple",
+            "type_file_accepted": [],
+            "metadonnees": [
+                {
+                    "id": 1,
+                    "label": "Métadonnée Tuple 1",
+                    "type": "text",
+                    "name": "metatuple1",
+                    "value": "",
+                },
+                {
+                    "id": 2,
+                    "label": "Métadonnée Tuple 2",
+                    "type": "text",
+                    "name": "metatuple2",
+                    "value": "",
+                }
+            ]
+        },
+        {
+            "id": 6,
+            "label": "Arbre",
+            "type_file_accepted": ["application/json"],
+            "metadonnees": [
+                {
+                    "id": 1,
+                    "label": "Métadonnée Arbre 1",
+                    "type": "text",
+                    "name": "metaarbre1",
+                    "value": "",
+                },
+                {
+                    "id": 2,
+                    "label": "Métadonnée Arbre 2",
+                    "type": "text",
+                    "name": "metaarbre2",
+                    "value": "",
+                }
+            ]
+        },
+        {
+            "id": 7,
+            "label": "Binaire",
+            "type_file_accepted": [],
+            "metadonnees": [
+                {
+                    "id": 1,
+                    "label": "Métadonnée Binaire 1",
+                    "type": "text",
+                    "name": "metabinaire1",
+                    "value": "",
+                },
+                {
+                    "id": 2,
+                    "label": "Métadonnée Binaire 2",
+                    "type": "text",
+                    "name": "metabinaire2",
                     "value": "",
                 }
             ]
