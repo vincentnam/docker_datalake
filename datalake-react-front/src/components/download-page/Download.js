9bfc4aaab73b4eb57804f4a9800ef7eb2af2cc47
TheoLGG <tlegagneur@myges.fr>
2021-06-11 16:37:50 +0200

    #55 change import api

--- datalake-react-front/src/components/download-page/Download.js
+++ datalake-react-front/src/components/download-page/Download.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Header } from '../Header';
 import { RowItem } from './RowItem';
-import api from '../api/api';
+import api from '../../api/api';
 import $ from 'jquery';
 import { Filters } from "./Filters";
 import moment from "moment";
