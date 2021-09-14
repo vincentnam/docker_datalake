9da7cbdf94a3e3258ce15648002cff2ff9f06193
TheoLGG <tlegagneur@myges.fr>
2021-09-14 09:52:20 +0200

    #94 Add load model in add and edit models component

--- service_zone/frontend/datalake-react-front/src/components/model/ModelAddForm.js
+++ service_zone/frontend/datalake-react-front/src/components/model/ModelAddForm.js
@@ -1,4 +1,5 @@
 import React from "react";
+import api from '../../api/api';
 
 export class ModelAddForm extends React.Component {
     constructor(props) {
@@ -9,6 +10,28 @@ export class ModelAddForm extends React.Component {
             newModel: {},
         };
     }
+    loadModel() {
+        api.get('models')
+            .then((response) => {
+                this.setState({
+                    models: response.data.models
+                });
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+
+    submitModels(){
+        api.post('models/add')
+            .then((response) => {
+                console.log(response)
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+
     handleChange(event) {
         const target = event.target;
         const value = target.type === 'checkbox' ? target.checked : target.value;
--- service_zone/frontend/datalake-react-front/src/components/model/ModelEditForm.js
+++ service_zone/frontend/datalake-react-front/src/components/model/ModelEditForm.js
@@ -1,4 +1,5 @@
 import React from "react";
+import api from '../../api/api';
 
 export class ModelEditForm extends React.Component {
     constructor(props) {
@@ -8,6 +9,27 @@ export class ModelEditForm extends React.Component {
             model: {},
             newModel: {},
         };
+    }
+    loadModel() {
+        api.get('models')
+            .then((response) => {
+                this.setState({
+                    models: response.data.models
+                });
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+
+    submitModels(){
+        api.put('models/edit')
+            .then((response) => {
+                console.log(response)
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
     }
     handleChange(event) {
         const target = event.target;
