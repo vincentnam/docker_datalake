c096297a7b965c8e20259c5bc371b187e2da4e01
Latamen <latamen.ait.meddour@modis.com>
2021-11-18 17:56:48 +0100

    #108 - Finished refacto SSH download of large data - Fixed date on IHM

--- service_zone/backend/flask/neocampus/routers/swift_file.py
+++ service_zone/backend/flask/neocampus/routers/swift_file.py
@@ -5,8 +5,6 @@ import base64
 from flask import Blueprint, jsonify, current_app, request, send_from_directory
 from ..services import swift, mongo
 import os
-import paramiko
-from pathlib import Path
 
 swift_file_bp = Blueprint('swift_file_bp', __name__)
 
@@ -79,54 +77,3 @@ def storage():
                         content_type, mongodb_url, other_data)
 
     return jsonify({"response": "Done !"})
-
-@swift_file_bp.route('/register-sge-file', methods=['POST'])
-def sge_file():
-    sge_ip_address = "10.200.156.253"
-    host_key = "AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBHJdWchwbWG661xt2c53FP2LRXV3vwJ0OaqCpkVe7LoYmuD8BlfxgDVvDt5ZtveMaKu4XjmR7DRY6tV2i7KhDOw="
-    ssh = paramiko.SSHClient()
-    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-
-    from base64 import decodebytes
-
-    transport = paramiko.Transport(sge_ip_address)
-    transport.get_security_options().key_types = ["ecdsa-sha2-nistp256"]
-
-    #keydata = b""""""
-    keydata = current_app.config['SSH_KEY']
-    key = paramiko.ECDSAKey(data=decodebytes(keydata))
-
-    ssh.load_system_host_keys()
-    ssh.connect(
-        sge_ip_address, 
-        username=current_app.config['SGE_USERNAME'], 
-        password=current_app.config['SGE_PASSWORD']
-    )
-    sftp = ssh.open_sftp()
-    filename = ""
-    localpath = '/home/'
-    remotepath = current_app.config['SGE_REMOTE_PATH']+filename
-    sftp.get(remotepath, localpath)
-    sftp.close()
-    ssh.close()
-
-    data_file = Path(localpath).read_text()
-    filename = data_file.name
-
-    data_file = base64.b64decode(data_file)
-    file_content = data_file
-
-    container_name = "neOCampus"
-    mongodb_url = current_app.config['MONGO_URL']
-    user = current_app.config['SWIFT_USER']
-    key = current_app.config['SWIFT_KEY']
-    authurl = current_app.config['SWIFT_AUTHURL']
-    content_type = "application/octet-stream"
-    application = None
-    data_process = "default"
-    processed_data_area_service = ["MongoDB"]
-    other_data = "sge_data"
-    
-    mongo.insert_datalake(file_content, user, key, authurl, container_name, filename,
-                        processed_data_area_service, data_process, application,
-                        content_type, mongodb_url, other_data)
--- service_zone/backend/flask/neocampus/services/swift.py
+++ service_zone/backend/flask/neocampus/services/swift.py
@@ -3,7 +3,9 @@ import os
 import swiftclient
 from flask import current_app
 from .mongo import get_swift_original_object_name
-
+from ..services import mongo
+import paramiko
+from pathlib import Path
 
 def download_object_file(container_name, object_id):
     """
@@ -26,3 +28,64 @@ def download_object_file(container_name, object_id):
 
     return file_path
 
+def sge_file(
+    sge_ip_address,
+    username,
+    password,
+    remote_path,
+    filename
+    ):
+    try:
+        ssh = paramiko.SSHClient()
+
+        # For host key - Add it automatically to known_hosts
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+        ssh.load_system_host_keys()
+
+        # Connect to server throughout SSH protocol
+        ssh.connect(
+            sge_ip_address, 
+            username=username, 
+            password=password
+        )
+        sftp = ssh.open_sftp()
+        # Localpath -> Locally in Docker container
+        localpath = '/home/'+filename
+
+        # Remote path on the server to find the file
+        remotepath = remote_path+filename
+
+        # Get and download it locally
+        sftp.get(remotepath, localpath)
+        sftp.close()
+        ssh.close()
+
+        path =  Path(localpath)
+        content_file = path.read_text()
+        filename = path.name
+
+        #data_file = base64.b64decode(content_file)
+        file_content = content_file
+
+        # All variables to put informations in MongoDB
+        # and in OpenstackSwift
+        container_name = "neOCampus"
+        mongodb_url = current_app.config['MONGO_URL']
+        user = current_app.config['SWIFT_USER']
+        key = current_app.config['SWIFT_KEY']
+        authurl = current_app.config['SWIFT_AUTHURL']
+        content_type = "application/octet-stream"
+        application = None
+        data_process = "default"
+        processed_data_area_service = ["MongoDB"]
+        other_data = "sge_data"
+        
+        mongo.insert_datalake(file_content, user, key, authurl, container_name, filename,
+                            processed_data_area_service, data_process, application,
+                            content_type, mongodb_url, other_data)
+
+        return "OK"
+    except Exception as e:
+        print(e)
+        return "Problem"
