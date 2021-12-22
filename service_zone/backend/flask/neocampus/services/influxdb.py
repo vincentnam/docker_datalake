e2af76df2af3f3c14d63443f7ec7e4fd812d8ce1
yousra.amrani_adm <yousra.amrani_adm@CBV-L0103.production.infra>
2021-12-22 16:52:23 +0100

    #104 add functions get_anomaly and insert_anomaly

--- service_zone/backend/flask/neocampus/services/influxdb.py
+++ service_zone/backend/flask/neocampus/services/influxdb.py
@@ -1,7 +1,10 @@
-from flask import current_app
+from flask import current_app,jsonify
 from influxdb_client import InfluxDBClient, Dialect
 import pandas as pd
 from datetime import datetime, timedelta
+from ..services import influxdb, mongo
+
+import json
 
 def get_handled_data(params):
     #Connection Influxdb
@@ -68,4 +71,150 @@ def connection_inflxdb():
     url = current_app.config['INFLUXDB_URL']
     org = current_app.config['INFLUXDB_ORG']
     client = InfluxDBClient(url=url, token=token, debug=True)
-    return client, org;
+    return client, org
+
+def get_all_measurements(bucket):
+    client, org = connection_inflxdb()
+    # Query for show all measurements in a bucket
+    query = f""" 
+    import \"influxdata/influxdb/schema\"
+
+    schema.measurements(bucket: \"{bucket}\")
+    """
+    # Execute the query
+    query_api = client.query_api()
+    tables = query_api.query(query=query, org=org)
+
+    # Flatten output tables into list of measurements
+    measurements = [row.values["_value"] for table in tables for row in table]
+
+    
+    return measurements
+
+def get_data_anomaly(Jour_1, Jour_2) :
+    """
+    get_data_anomaly from influxdb between 2 dates 
+    :param Jour_1:  
+    :param Jour_2:
+    :return: Anomaly (dict)
+    """
+    client, org = influxdb.connection_inflxdb()
+
+    bucket = "neOCampus"
+    startDate = (datetime.now() - timedelta(days = Jour_2) ).isoformat() + "Z"
+    endDate = (datetime.now() - timedelta(days=Jour_1) ).isoformat() + "Z"
+    
+    measurements = influxdb.get_all_measurements(bucket)
+    anomalies = []
+    # Execute the query
+    query_api = client.query_api()
+    for measurement in measurements :
+        tables = query_api.query(f'''
+        from(bucket: \"{bucket}\") |> range(start: {startDate}, stop: {endDate})
+            |> filter(fn: (r) => r["_measurement"] == \"{measurement}\")
+            |> group(columns: ["topic"])
+        ''', org=org)
+        topics = []
+        # Parsing the result for return the list of topics
+        for table in tables:
+            for record in table.records:
+                # Verification if the topic is in the list
+                if record.values.get("topic") not in topics:
+                    topics.append(str(record.values.get("topic")))
+
+        all_topics = {
+            "topics": topics
+        }
+        for topic in all_topics["topics"] :
+            result = query_api.query_data_frame(f'''
+            from(bucket: \"{bucket}\")
+                |> range(start: {startDate}, stop: {endDate})
+                |> filter(fn: (r) => r["_measurement"] == \"{measurement}\" and r["topic"] == \"{topic}\")
+                |> group(columns: ["_time"])
+            ''', org=org)
+
+            #Dataframe to json format
+            data = result.to_json(orient="index")
+            data = json.loads(data)
+            all_data = []
+            for key, value in data.items() :
+                all_data.append(value)
+
+            for data in all_data :
+                if data["_value"] == 0 :
+                    new_anomaly_1 = {   
+                        "topic": topic,
+                        "value": int(data["_value"]),
+                        "unit": measurement,
+                        "datetime": data["_time"]/1000 - 86400, 
+                        "startDate_detection": startDate,
+                        "endDate_detection": endDate,
+                        "nbr" : 1
+                    }
+                    new_anomaly_2 = {   
+                        "topic": topic,
+                        "value": int(data["_value"]),
+                        "unit": measurement,
+                        "datetime": data["_time"]/1000 - 172800,
+                        "startDate_detection": startDate,
+                        "endDate_detection": endDate,
+                        "nbr" : 2
+                    }
+                    if new_anomaly_1 in anomalies :
+                        anomalies.append(
+                            {   
+                                "topic": topic,
+                                "value": int(data["_value"]),
+                                "unit": measurement,
+                                "datetime": data["_time"]/1000 - 86400,
+                                "startDate_detection": startDate,
+                                "endDate_detection": endDate,
+                                "nbr" : 2
+                            }
+                        )
+                    elif new_anomaly_2 in anomalies :
+                        anomalies.append(
+                            {   
+                                "topic": topic,
+                                "value": int(data["_value"]),
+                                "unit": measurement,
+                                "datetime": data["_time"]/1000 - 172800,
+                                "startDate_detection": startDate,
+                                "endDate_detection": endDate,
+                                "nbr" : 3
+                            }
+                        )
+                    else :
+                        anomalies.append(
+                            {   
+                                "topic": topic,
+                                "value": int(data["_value"]),
+                                "unit": measurement,
+                                "datetime": data["_time"]/1000,
+                                "startDate_detection": startDate,
+                                "endDate_detection": endDate,
+                                "nbr" : 1
+                            })
+    print(anomalies)
+    last_anomaly =[]
+    for ano in anomalies :
+        if ano['nbr'] == 3 : 
+            last_anomaly.append(
+                {   
+                                "topic": ano['topic'],
+                                "value": int(ano["value"]),
+                                "unit": ano['unit'],
+                                "datetime": str(datetime.fromtimestamp(ano["datetime"]).strftime("%Y-%m-%dT%H:%M:%S.%fZ")),
+                                "startDate_detection": startDate,
+                                "endDate_detection": endDate,
+                            }
+            )
+    print(last_anomaly)
+    anomaly = {
+        "anomaly" : last_anomaly
+    }
+
+    result = mongo.insert_anomaly(anomaly,endDate)
+    #print("for influx db")
+    #print(anomaly)
+    return anomaly
--- service_zone/backend/flask/neocampus/services/mongo.py
+++ service_zone/backend/flask/neocampus/services/mongo.py
@@ -1,4 +1,3 @@
-import datetime
 from flask import current_app
 from pymongo import MongoClient
 from bson.objectid import ObjectId
@@ -7,6 +6,8 @@ from swiftclient.service import SwiftService
 import datetime
 from bson.json_util import dumps
 from datetime import datetime as dt
+from time import sleep
+
 
 
 def get_swift_original_object_name(swift_container_name, swift_object_id):
@@ -58,7 +59,7 @@ def get_metadata(db_name, params):
         metadata = metadata.skip(params['offset'])
         metadata = metadata.limit(params['limit'])
 
-    nb_objects = metadata.count()
+    nb_objects = metadata.count() 
 
     return nb_objects, metadata
 
@@ -281,3 +282,70 @@ def update_model(param):
     result = "done"
     return result
 
+
+def insert_anomaly(anomaly,endDate):
+    """
+    insert_anomaly from influx db to mongodb 
+    :param anomaly:
+    :param endDate:
+    :return: done
+    """
+    mongodb_url = current_app.config['MONGO_URL']
+    anomalie = anomaly
+    mongo_client = MongoClient(mongodb_url).data_anomaly.influxdb_anomaly
+    data_histor = mongo_client.find()
+    new_date = endDate #datetime.datetime.now().isoformat() + "Z"
+        
+    for ano in anomalie["anomaly"] :
+        ano["startDate_detection"] = datetime.datetime.strptime(str(ano["startDate_detection"]), "%Y-%m-%dT%H:%M:%S.%fZ")
+        ano["endDate_detection"] = datetime.datetime.strptime(str(ano["endDate_detection"]), "%Y-%m-%dT%H:%M:%S.%fZ")
+        ano['datetime'] = datetime.datetime.strptime(ano['datetime'], "%Y-%m-%dT%H:%M:%S.%fZ")
+
+    anomaly_without_id =[]
+    data_without_id = {'objects': []}
+    for row in data_histor:
+        data_without_id['objects'].append({
+            "topic": row['topic'],
+            "value": row['value'],
+            "unit": row['unit'],
+            'datetime': row['datetime']
+        })
+    for ano in anomalie["anomaly"] :
+        anomaly_without_id.append({
+            "topic": ano['topic'],
+            "value": ano['value'],
+            "unit": ano['unit'],
+            'datetime': ano['datetime']
+        })
+    new_anomaly = {'objects': []}
+    data = mongo_client.find()
+    if data_histor.count() != 0:
+        for ano in anomaly_without_id :
+            data = mongo_client.find()
+            for row in data:
+                if ano in data_without_id['objects'] :
+                    if ano['datetime'] == row['datetime'] and ano['topic'] == row ['topic'] and ano['unit'] == row ['unit'] :
+                        print("update ici ")
+                        mongo_client.update_one({"_id":row ['_id']},{"$set":{"endDate_detection": datetime.datetime.strptime(str(new_date), "%Y-%m-%dT%H:%M:%S.%fZ")}})
+                else :
+                    for ano_add in anomalie["anomaly"] :
+                        if ano_add not in new_anomaly['objects'] :
+                            if ano['datetime'] == ano_add['datetime'] and ano['topic'] == ano_add ['topic'] and ano['unit'] == ano_add ['unit'] :
+                                print("insert ici ")
+                                new_anomaly['objects'].append({
+                                    "topic": ano_add['topic'],
+                                    "value": ano_add['value'],
+                                    "unit": ano_add['unit'],
+                                    'datetime': ano_add['datetime'],
+                                    'endDate_detection': ano_add['endDate_detection'],
+                                    'startDate_detection': ano_add['startDate_detection']
+                                })
+
+        for ano in new_anomaly['objects'] :
+            print("new anomaly ")
+            mongo_client.insert_one(ano)  
+    else :
+        for ano in anomalie["anomaly"] :
+            mongo_client.insert_one(ano)            
+    result = "done"
+    return result
