5cdd136271bd2495b8b555cac093c0a477222a17
TheoLGG <tlegagneur@myges.fr>
2022-04-20 18:49:46 +0200

    #138 Login connection to openstack

--- service_zone/backend/flask/neocampus/__init__.py
+++ service_zone/backend/flask/neocampus/__init__.py
@@ -1,21 +1,19 @@
 import os
 
 from flask import Flask
-from flask_keystone import FlaskKeystone
-from flask_oslolog import OsloLog
-from oslo_config import cfg
-from oslo_context import context
+# from flask_keystone import FlaskKeystone
+# from flask_oslolog import OsloLog
+# from oslo_config import cfg
+# from oslo_context import context
 from . import routers
 
-key = FlaskKeystone()
-log = OsloLog()
-cfg.CONF(default_config_files=['neocampus/config.ini'])
+# key = FlaskKeystone()
+# cfg.CONF(default_config_files=['neocampus/config.ini'])
 
 def create_app():
     app = Flask(__name__)
 
-    log.init_app(app)
-    key.init_app(app)
+#     key.init_app(app)
     app.config.from_pyfile("config.py")
     routers.init_app(app)
 
--- service_zone/backend/flask/neocampus/routers/keystone_router.py
+++ service_zone/backend/flask/neocampus/routers/keystone_router.py
@@ -1,17 +1,27 @@
 import os
 import uuid
 import base64
-from flask import Blueprint, jsonify, current_app, request, send_from_directory, make_response
+from flask import Blueprint, jsonify, current_app, request
 import os
 from ..services import keystone
 from keystoneauth1 import session as keystone_session
 from keystoneauth1.identity import v3
-from keystoneclient.v3 import client
+from keystoneauth1 import token_endpoint
 
 keystone_router_bp = Blueprint('keystone_router_bp', __name__)
 
 @keystone_router_bp.route("/hello", methods=['GET'])
 def hello():
+    """
+    ---
+    get:
+        description: Test
+        responses:
+            '200':
+                description: call successful
+        tags:
+            - keystone_router
+    """
     ctx = keystone.get_oslo_context()
     return f'Hello, {ctx.user_id} on project {ctx.project_id}!'
 
@@ -30,33 +40,23 @@ def login():
     """
     user = request.get_json()['user']
     password = request.get_json()['password']
+    print(user)
+    print(password)
+    print(current_app.config['KEYSTONE_URL'])
 
     auth = v3.Password(
         auth_url=current_app.config['KEYSTONE_URL'],
         username=user,
         password=password,
-        project_id='some-project',
-        user_domain_name='default',
-        project_domain_name='default',
+        project_id=current_app.config['PROJECT_ID'],
+        user_domain_id=current_app.config['USER_DOMAIN_ID']
     )
+    #auth = token_endpoint.Token(current_app.config['KEYSTONE_URL'],token="token")
 
-    session = keystone_session.Session(
-        auth=auth,
-        user_agent='some-app',
-    )
 
-    client = client.Client(
-        session=session(),
-        interface='public',
-    )
-
-    service = client.services.list(type='some-keystone-service-type')[0]
-    endpoint = client.endpoints.list(service=service.id, interface='public', enabled=True)[0]
-    url = endpoint.url
-
-    print(session.get(f'{url}/test').json())
-    print(client.token.id)
-    return client.token.id
+    sess = keystone_session.Session(auth=auth)
+    #print(sess.get("/v3/users", endpoint_filter={'service_type': 'identity'}))
+    return sess.get_token()
 
 
 
