b9dea07ec2207c157594c7675f9b27a89f1dfcfa
TheoLGG <tlegagneur@myges.fr>
2022-10-10 15:48:02 +0200

    #156 Update column checkUpdate in false for all flux after nbupdate > 0

--- process_zone/apache_spark/src/main/scala/service/StreamGetter.scala
+++ process_zone/apache_spark/src/main/scala/service/StreamGetter.scala
@@ -2,10 +2,17 @@ package service
 
 import com.typesafe.config.Config
 import org.apache.avro.io.Encoder
+import org.apache.spark.sql.catalyst.dsl.expressions.StringToAttributeConversionHelper
+import org.apache.spark.sql.catalyst.encoders.RowEncoder
+import org.apache.spark.sql.functions.{col, regexp_replace, udf, when}
 import org.apache.spark.sql.{Dataset, Row, SparkSession}
+
+import java.util.Base64
 //import org.mongodb.scala._
 
 import scala.language.postfixOps
+
+import com.mongodb.spark._
 
 class StreamGetter(config: Config) {
 
@@ -31,13 +38,29 @@ class StreamGetter(config: Config) {
       .options(Map("uri" -> mongodbUri, "database" -> "mqtt", "collection" -> "flux")).load()
 
     val fluxCollectionStatusTrue = fluxCollection.where(fluxCollection("checkUpdate") === true && fluxCollection("status") === true)
-    var errors = 0
+    var nbUpdate = 0
     for (configFlux <- fluxCollectionStatusTrue.rdd.collect()) {
       if (configFlux(2).asInstanceOf[Boolean]) {
-        errors += 1
+        nbUpdate += 1
       }
     }
-    if (errors > 0) {
+    if (nbUpdate > 0) {
+      val configCollection = spark.read.format("com.mongodb.spark.sql.DefaultSource")
+        .options(Map("uri" -> mongodbUri, "database" -> "mqtt", "collection" -> "flux")).load()
+
+      val checkUpdateFalse = udf { (checkUpdate: Boolean) =>
+        if (checkUpdate) false else checkUpdate
+      }
+      configCollection.withColumn("checkUpdate", checkUpdateFalse(configCollection("checkUpdate")))
+
+      val confCol = configCollection.map(row => {
+        val row1: Boolean = row(2).asInstanceOf[Boolean]
+        val checkUpdate: Boolean = if (row1) false else row1
+        Row(row(0), row(1), checkUpdate, row(3), row(4), row(5), row(6), row(7), row(8), row(9))
+      })(configCollection.encoder)
+
+      MongoSpark.save(confCol.write.options(Map("uri" -> mongodbUri, "database" -> "mqtt", "collection" -> "flux")).option("replaceDocument", "true").mode("append"))
+
       return true
     } else {
       return false
