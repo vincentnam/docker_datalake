54d424efb02bd11044c4aafee2eec74635c167ec
vincentnam <dang.vincentnam@gmail.com>
2023-02-23 18:27:09 +0100

    #162 Add CORS policy handling in flask

--- service_zone/backend/flask/neocampus/__init__.py
+++ service_zone/backend/flask/neocampus/__init__.py
@@ -1,10 +1,10 @@
 import os
 from flask import Flask
 from . import routers
-
+from flask_cors import CORS
 def create_app():
     app = Flask(__name__)
-
+    CORS(app)
     app.config.from_pyfile("config.py")
     routers.init_app(app)
 
