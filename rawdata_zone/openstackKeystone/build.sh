3d91badf97a81f3c640cc19ef306709d837dc79d
vincentnam <dang.vincentnam@gmail.com>
2026-05-06 15:36:10 +0200

    Set version (2026.1 + Ubuntu Noble) for a better stability

--- rawdata_zone/openstackKeystone/build.sh
+++ rawdata_zone/openstackKeystone/build.sh
@@ -1,76 +1,71 @@
 #!/bin/bash
 set -e
 
+# ===================== CONFIGURATION =====================
+OPENSTACK_RELEASE="stable/2026.1"
+BASE_DISTRO="ubuntu:noble"
+BASE_TAG="ubuntu_noble"
+
+# Ceph : Option recommendedf for Noble
+CEPH_REPO=""
+# CEPH_REPO='deb https://download.ceph.com/debian-tentacle/ noble main'  # Alternative
+
+# ========================================================
+
 if [ ! -d "loci" ]; then
     echo "Clonage du dépôt OpenStack LOCI..."
     git clone https://github.com/openstack/loci.git
 fi
 
-BASE_DISTRO="ubuntu:jammy"
-BASE_DISTRO_NAME=$(printf '%s\n' "$BASE_DISTRO" | tr ':' '_')
-#echo "Distribution name and version : $BASE_DISTRO"
-#echo "Distribution tag : $BASE_DISTRO_NAME"
-
 cd loci
+
 echo "=========================================="
-echo "Démarrage du build des images LOCI"
+echo "Build Loci - OpenStack ${OPENSTACK_RELEASE} sur ${BASE_DISTRO}"
 echo "=========================================="
-#git checkout stable/2025.1
-# On ajoute PROFILES="python" pour que LOCI installe python3 dans l'image de base
-# On peut aussi ajouter "fluentd" ou d'autres si besoin plus tard.
 
-
-echo "Construction de l'image locale : base image  ($BASE_DISTRO)"
-
+# Construction de la base image
+echo "Construction base image (${BASE_DISTRO})"
 docker build . \
     -f Dockerfile.base \
     --build-arg FROM=$BASE_DISTRO \
-    --build-arg CEPH_REPO='deb https://download.ceph.com/debian-reef/ jammy main' \
-    --tag base:$BASE_DISTRO_NAME
+    --build-arg CEPH_REPO="${CEPH_REPO}" \
+    --tag base:${BASE_TAG}
 
-
-#    --build-arg PIP_WHEEL_ARGS="--no-build-isolation --no-cache-dir" \
-
-echo "Construction de l'image locale : requirements image"
-
- docker build . \
+# Requirements
+echo "Construction requirements image"
+docker build . \
     -f Dockerfile \
     --target requirements \
-    --build-arg FROM=base:$BASE_DISTRO_NAME \
+    --build-arg FROM=base:${BASE_TAG} \
     --build-arg PROJECT=requirements \
-    --build-arg PIP_WHEEL_ARGS="" \
-    --build-arg PIP_PACKAGES="setuptools==67.2.0 cython" \
-    --tag requirements:$BASE_DISTRO_NAME
+    --build-arg PROJECT_RELEASE=${OPENSTACK_RELEASE} \
+    --build-arg PIP_PACKAGES="setuptools wheel cython" \
+    --tag requirements:${BASE_TAG}-${OPENSTACK_RELEASE//stable\//}
 
-#    --build-arg PIP_PACKAGES="setuptools==67.2.0 XStatic-tv4==1.2.7.0 XStatic-term.js==0.0.7.0" \
-
-echo "Construction de l'image locale : my-keystone:local"
-#
-#
-## MASTER : need to change openstack version chosen
-##TODO : Set openstack version variable
-#
+# Keystone
+echo "Construction Keystone"
 docker build . \
-    --build-arg FROM=base:$BASE_DISTRO_NAME \
-    --build-arg WHEELS=requirements:$BASE_DISTRO_NAME\
+    -f Dockerfile \
+    --build-arg FROM=base:${BASE_TAG} \
+    --build-arg WHEELS=requirements:${BASE_TAG}-${OPENSTACK_RELEASE//stable\//} \
     --build-arg PROJECT=keystone \
+    --build-arg PROJECT_RELEASE=${OPENSTACK_RELEASE} \
+    --build-arg PROJECT_REF=${OPENSTACK_RELEASE} \
     --build-arg PROFILES=apache \
-    --build-arg PIP_OPTS="" \
     --build-arg DIST_PACKAGES="python3-openstackclient curl" \
-    --tag keystone:master-$BASE_DISTRO_NAME
+    --tag keystone:${OPENSTACK_RELEASE//stable\//}-${BASE_TAG}
 
-echo "Keystone construit."
-
-echo "Construction de l'image locale : my-horizon:local"
+# Horizon
+echo "Construction Horizon"
 docker build . \
-    --build-arg FROM=base:$BASE_DISTRO_NAME \
-    --build-arg WHEELS=requirements:$BASE_DISTRO_NAME\
+    -f Dockerfile \
+    --build-arg FROM=base:${BASE_TAG} \
+    --build-arg WHEELS=requirements:${BASE_TAG}-${OPENSTACK_RELEASE//stable\//} \
     --build-arg PROJECT=horizon \
+    --build-arg PROJECT_RELEASE=${OPENSTACK_RELEASE} \
+    --build-arg PROJECT_REF=${OPENSTACK_RELEASE} \
     --build-arg PROFILES=apache \
-    --build-arg PIP_PACKAGES=pymemcache \
-    --build-arg PIP_OPTS="" \
-    --tag horizon:master-$BASE_DISTRO_NAME
+    --build-arg PIP_PACKAGES="pymemcache" \
+    --tag horizon:${OPENSTACK_RELEASE//stable\//}-${BASE_TAG}
 
-echo "Keystone and Horizon build finished"
-
-#docker compose up
+echo "Build terminé !"
